a8ce2698e387dec638cfc92c114e33b49c6ed632
fer.belen <belenxmorales@gmail.com>
2023-08-18 10:39:13 -0300

    profile.ts

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,49 +1,50 @@
 import axios from 'axios';
-import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import Input from "@/components/Input";
 import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub} from 'react-icons/fa';
 
 const Auth = () => {
-    
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
-
-
     const [variant, setVariant] = useState('login');
 
+    
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login'? 'register' : 'login' );
     }, []);
 
-     const register = useCallback (async() => {
 
+const login = useCallback(async() => {
+        try {
+            await signIn('credentials', {
+             email,
+             password,
+             callbackUrl:'/profiles'   
+            });
+        } catch (error) {
+            console.log(error);
+        }
+     }, [email, password]);
+
+
+const register = useCallback (async() => {
         try {
             await axios.post('/api/register', {
                 email,
                 name,
                 password
             });
+            login();
           } catch (error) {
             console.log(error);
         }
-     }, [email, name, password]);
+     }, [email, name, password, login]);
 
 
-     const login = useCallback(async() => {
-        try {
-            await signIn('credentials', {
-             email,
-             password,
-             redirect:false,
-             callbackUrl:'/'   
-            });
-        } catch (error) {
-            console.log(error);
-        }
-     }, [email, password]);
+     
 
 
 
@@ -61,7 +62,7 @@ const Auth = () => {
                         </h2>
                         <div className="flex flex-col gap-4">
                           {variant === 'register' && (
-                            <Input
+                         <Input
                                 label="Username"
                                 onChange={(ev: any) => setName(ev.target.value)}
                                 id="name"
@@ -88,7 +89,7 @@ const Auth = () => {
                         </button>
                         <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
                             <div 
-                              onClick={() => signIn('google', { callbackUrl: '/' })} 
+                              onClick={() => signIn('google', { callbackUrl: '/profiles' })} 
                                className='
                                 w-10
                                 h-10
@@ -105,7 +106,7 @@ const Auth = () => {
                                 <FcGoogle size={30} />
                              </div>
                             <div 
-                              onClick={() => signIn('github', { callbackUrl: '/' })} 
+                              onClick={() => signIn('github', { callbackUrl: '/profiles' })} 
                                 className='
                                 w-10
                                 h-10
